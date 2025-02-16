f8067116cfc8c60adc6e7a1821035cf3dcc1fa62
Miguel Cabrerizo <doncicuto@gmail.com>
2025-02-16 12:36:52 +0100

    fix: index.db location

--- internal/models/winget/deploy.go
+++ internal/models/winget/deploy.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/open-uem/openuem-console/internal/views/partials"
@@ -104,9 +105,10 @@ func CountPackages(packageName string, indexPath string) (int, error) {
 
 func OpenWingetDB(indexPath string) (*sql.DB, error) {
 	// Open Winget Community Repository index database
-	if _, err := os.Stat(indexPath); os.IsNotExist(err) {
+	dbPath := filepath.Join(indexPath, "index.db")
+	if _, err := os.Stat(dbPath); os.IsNotExist(err) {
 		return nil, fmt.Errorf("database doesn't exist, reason: %v", err)
 	}
 
-	return sql.Open("sqlite3", indexPath)
+	return sql.Open("sqlite3", dbPath)
 }
